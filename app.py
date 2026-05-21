6fcd747b9ef81b75bc149c39b2e781645340ba8e
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-05-21 19:46:58 -0300

    Adiciona estilo para as páginas, melhora código, e cria template base

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
+# Necessário para usar mensagens 'flash' (sessões)
+app.secret_key = 'uma_chave'
+
 lista = []
+
 
 @app.route('/', methods=['GET', 'POST'])
 def recado():
     if request.method == 'POST':
         autor = request.form.get('autor')
         mensagem = request.form.get('mensagem')
-        lista.append({"Autor": autor, "Mensagem": mensagem})
-        print(lista)
-        return redirect(url_for("recado"))
-    
-    # Se for GET (o usuário acabou de acessar), mostra o formulário
-    return render_template('index.html', recado=lista)
+
+        # Guardando em snake_case (padrão)
+        lista.append({"autor": autor, "mensagem": mensagem})
+
+        # Fornece um feedback para o usuário na próxima página
+        flash("Recado enviado com sucesso!")
+        return redirect(url_for("listar"))  # Redireciona direto para o mural
+
+    return render_template('index.html')
+
 
 @app.route("/recados", methods=['GET'])
 def listar():
     return render_template('recados.html', lista=lista)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
